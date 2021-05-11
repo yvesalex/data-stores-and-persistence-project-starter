7eb001ced3794385e512053e4192a0ea90d280e5
Yves Alexandre Cadet <y.a.cadet@hotmail.com>
2021-05-10 21:35:33 -0400

    last updates before 2nd submission

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -13,5 +14,12 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findByEmployeeIds(long employeeId);
 
+    @Query(value = "select distinct s.id, s.date " +
+        " from schedule s " +
+        " left join schedule_activities sa on s.id = sa.schedule_id" +
+        " left join schedule_pet_ids sp on s.id = sp.schedule_id " +
+        " left join pet_data p on p.id = sp.pet_ids " +
+        " where p.owner_id = :customerId", nativeQuery = true)
+    @Transactional
     List<Schedule> findByCustomerId(long customerId);
 }
